da0c0f0182b2764b17135a770c598fb6e352bb7d
Carlos Apolaya <temis_it@hotmail.com>
2023-08-13 19:09:11 -0500

    new web

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, NavBar, Stats, Testimonials, Hero } from "./components";
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+
+<div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { close, logo, menu } from "../assets";
+
+
+const NavBar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+          <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+          </nav>
+
+  );
+}
+
+export default NavBar;
+
