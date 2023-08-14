f07b546c68be79f41830250ce45de5d8a1e996d7
Carlos Apolaya <temis_it@hotmail.com>
2023-08-14 17:48:53 -0500

    asdf

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -23,7 +23,7 @@ const Billing = () => (
       <div className="flex flex-row flex-wrap sm:mt-5 mt-6">
       <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
 
-        <img src={google} alt="google_play" className="w-[144.17px] h-[73.08px] object-contain cursor-pointer" />
+        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
       </div>
     </div>
   </section>
