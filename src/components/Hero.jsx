e5bd0d735d3d66bab4b435c382526cbf9c4197f5
Carlos Apolaya <temis_it@hotmail.com>
2023-08-14 13:41:56 -0500

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,14 @@ const Hero = () => { // Agrega () para que sea una función y devuelva JSX
       </div>
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
       <img src={robot} alt="billing" className="w-[100%] h-[90%] relative z-[5]" />
+      
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+              <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+              <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
 
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
